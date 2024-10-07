2db407fba5b042637bf9ada693ed8a4dd522ed5a
H-goto16 <h.goto.engineer@gmail.com>
2024-10-07 11:15:26 +0900

    fix: thread number

--- 2024-1007/openmp-1.c
+++ 2024-1007/openmp-1.c
@@ -5,7 +5,7 @@ int main(void) {
         int n = 2000000000;
         double sum = 0.0;
 
-        # pragma omp parallel for reduction(+:sum) num_threads(16)
+        # pragma omp parallel for reduction(+:sum) num_threads(4)
         for (int i = 1; i < n; i++) {
                 sum += 1.0 / (i + 1);
         }
@@ -13,4 +13,4 @@ int main(void) {
         printf("Sum = %f\n", sum);
 
         return 0;
-}
+
