f8a49529fc8b4478f01299bf37c154fdbddbca9c
H-goto16 <h.goto.engineer@gmail.com>
2024-11-17 22:57:44 +0900

    feat: add 17

---
+++ 2024-1117/matmul-4-2.c
@@ -0,0 +1,50 @@
+#include <omp.h>
+#include <stdio.h>
+
+#define N 2000
+
+int A[N][N], B[N][N], C[N][N];
+
+void initialize(void) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A[i][j] = B[i][j] = (i == j);
+    }
+  }
+}
+
+void multiply(void) {
+  omp_set_nested(1);
+#pragma omp parallel for num_threads(2)
+  for (int i = 0; i < N; i++) {
+#pragma omp parallel for num_threads(2)
+    for (int j = 0; j < N; j++) {
+      C[i][j] = 0;
+      for (int k = 0; k < N; k++) {
+        C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+}
+void check(void) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (C[i][j] != (i == j)) {
+        printf("Error found: C[%d][%d] = %d\n", i, j, C[i][j]);
+        break;
+      }
+    }
+  }
+}
+
+int main(void) {
+  initialize();
+  double start_time = omp_get_wtime();
+  multiply();
+  double end_time = omp_get_wtime();
+  check();
+
+  printf("Time for matrix multiplication: %.10f seconds\n",
+         end_time - start_time);
+  return 0;
+}
